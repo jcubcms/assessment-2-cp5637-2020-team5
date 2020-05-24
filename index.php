6b1d4a2898c6a9e114bdf4e19248fc8bc0968c22
hunt250994 <varunvyas.1994@gmail.com>
2020-05-25 03:36:36 +1000

    Just another test commit as because commits are not being marked on github

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * The main template file
  *
- * This is the most generic template file in a WordPress theme
+ *
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
@@ -12,45 +12,34 @@
  * @package _s
  */
 
-get_header();
-?>
+get_header(); ?>
 
-	<main id="primary" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main blog-posts-wrapper" role="main">
+			<div class="col-3">
 				<?php
-			endif;
+				if ( have_posts() ) :
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
+					endwhile;
+				else :
 
-			the_posts_navigation();
+					get_template_part( 'template-parts/content', 'none' );
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+				endif; ?>
+			</div><!-- .blog-archive-wrapper -->
+			<?php the_posts_navigation(); ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php
 get_sidebar();
